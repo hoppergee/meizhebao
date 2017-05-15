f4866ca654f6b2566a4aa52eff485a92e2bdd642
Ji Xiang <jx4554@gmail.com>
2017-05-15 14:44:37 +0800

    finish signin/signout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,7 @@
 //= require turbolinks
 //= require bootstrap/alert
 //= require bootstrap/dropdown
+//= require bootstrap/tab
 //= require_tree .
+
+
---
+++ app/assets/javascripts/sign.js
@@ -0,0 +1,28 @@
+// $(document).ready(function(){
+// //========== Sign in / Sign out ===========
+
+//   $('#myTabs a').click(function (e) {
+//     e.preventDefault()
+//     $(this).tab('show')
+//   })
+
+
+//   $(".sign-tabs tab").click(function() {
+
+//     if ($(this).hasClass('signin')) {
+//       $('.sign-tabs .tab').removeClass('active');
+//       $(this).addClass('active');
+//       $('.info').hide();
+//       $('.signin-info').show();
+//       console.log("111111");
+//     }
+//     if ($(this).hasClass('signup')) {
+//       $('.sign-tabs .tab').removeClass('active');
+//       $(this).addClass('active');
+//       $('.info').hide();
+//       $('.signup-info').show();
+//       console.log("2222");
+//     }
+//   });
+
+// });
