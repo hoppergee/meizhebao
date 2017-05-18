7ed1e84e41fa9eff232f1093446e45d715fd5fe9
Ji Xiang <jx4554@gmail.com>
2017-05-18 12:08:06 +0800

    :sparkles create dress shop index

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,11 +17,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [800, 800]
 
   version :thumb do
-    process resize_to_fit: [200, 200]
+    process resize_and_pad: [200, 200]
   end
 
   version :medium do
-    process resize_to_fit: [400, 400]
+    process resize_and_pad: [600, 600]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
