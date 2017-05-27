881d11c115f32ae71b1629cae3a0b0a30472b276
Ji Xiang <jx4554@gmail.com>
2017-05-27 18:13:27 +0800

    add ship/shipped/canel/return action to admin/orders_controller

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,14 @@ Rails.application.routes.draw do
 	end
 
 	namespace :account do
-		resources :orders
+		resources :orders do
+			member do
+				post :cancel
+				post :ship
+				post :shipped
+				post :return
+			end
+		end
 	end
 
 end
