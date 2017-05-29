7cfe5b71d3e045d6ea34602d923bd15605624750
Ji Xiang <jx4554@gmail.com>
2017-05-29 14:52:44 +0800

    add en/cn switcher in footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :set_locale
+
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
+
   helper_method :current_cart
 
   def current_cart
